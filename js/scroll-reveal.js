3807260f09de4ad1fb5973cee9767c13b24ec486
Lassavins <lassavins@gmail.com>
2022-04-13 18:45:38 +0200

    primer c ommit

---
+++ js/scroll-reveal.js
@@ -0,0 +1,32 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.movido', {
+    duration: 1000,
+    origin: 'top',
+    distance: '100px',
+    interval: 100
+})
+
+sr.reveal('.punto', {
+    duration: 3000,
+    distance: '0px',
+    interval: 100
+})
+
+
+
+sr.reveal('.conDaniel', {
+    duration: 1000,
+    origin: 'top',
+    distance: '0px',
+    interval: 100,
+    delay: 1000
+})
+
+sr.reveal('.preparador', {
+    duration: 1000,
+    origin: 'top',
+    distance: '0px',
+    interval: 100,
+    delay: 1500
+})
